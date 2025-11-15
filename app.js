09dba8c7d9e22736158b4b0f0e309dda80bbc288
Burak <fatihvenom@gmail.com>
2025-11-15 05:16:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById("cal").innerText = localStorage.getItem("calorie") || "0 kcal";
+document.getElementById("water").innerText = localStorage.getItem("water") || "0 ml";
+document.getElementById("workout").innerText = localStorage.getItem("workout") || "0 dk";
